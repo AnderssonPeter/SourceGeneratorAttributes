636ef43c1f4a2331248aef8dc665df757dc4d1cc
AnderssonPeter <1308309+AnderssonPeter@users.noreply.github.com>
2023-01-08 23:11:01 +0100

    Add project files.

---
+++ SourceGeneratorAttributes/Generator.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace SourceGeneratorAttributes;
+
+
+[Generator]
+public class Generator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var output = @"using System;
+namespace SourceGeneratorAttributes;
+[AttributeUsage(AttributeTargets.Assembly, Inherited = false)]
+class GlobalAttribute : Attribute
+{
+    public int OptionalNumber { get; set; } = 10;
+    public string OptionalString { get; set; } = ""test"";
+    public GlobalAttribute(string requiredString, int requiredNumber) {}
+}
+";
+        context.AddSource("GlobalAttribute.cs", SourceText.From(output, Encoding.UTF8));
+
+        var attributes = context.Compilation.Assembly.GetAttributes();
+        foreach (var attribute in attributes)
+        {
+            Console.WriteLine(attribute);
+        }
+    }
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SourceGeneratorAttributes;
+
+[assembly:Global("text", 20, OptionalNumber = 33, OptionalString = "random")]
+Console.WriteLine("Hello, World!");
